974791d549529b90c2e4ebf4e72c1fbcd5dedd96
Lucas-FernandoJesus <lucasfernandojesus2@gmail.com>
2023-09-21 09:43:14 -0300

    Jogo da Adivinhacao Finalizado com Sucesso

--- Projetos Alura/Projeto Jogo da Adivinhacao/Projeto_adivinhacao.cpp
+++ Projetos Alura/Projeto Jogo da Adivinhacao/Projeto_adivinhacao.cpp
@@ -1,22 +1,57 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main (){
 
+    /*MENU*/
+
     cout << "--------------------------------------" << endl;
     cout << "---BEM VINDO AO JOGO DE ADIVINHACAO---" << endl;
     cout << "--------------------------------------" << endl;
 
-    const int NUM_SECRET = 5;
+    cout << "Escolha o seu nivel de dificuldade : " <<endl;
+    cout << "As opcoes sao : " <<endl;
+    cout << "Facil (F), Medio (M), Dificil (D)" << endl;
+
+    char dificuldade;
+    cin >> dificuldade;
+    
+    int numero_de_tentativas;
+
+    /*CONDICOES*/
+
+    if(dificuldade == 'F'){
+        numero_de_tentativas = 15;
+    }
+    else if(dificuldade == 'M'){
+        numero_de_tentativas = 10;
+    }
+    else if (dificuldade == 'D'){
+        numero_de_tentativas = 5;
+    }
+    else{
+        cout << "Entrada invalida, digite uma opcao valida !" <<endl;
+        return 0;
+    }
+
+    /*NUMERO ALEATORIO*/
+
+    srand(time(0));
+    const int NUM_SECRET = rand() % 100;
 
     bool nao_acertou = true;
     int tentativas = 0;
 
+    /*PONTUACAO*/
+
     double pontos = 1000.0;
 
-    while(nao_acertou){
-        tentativas ++;
+    /*LOOP*/
+
+    for(tentativas = 1; tentativas <= numero_de_tentativas; tentativas++){
         int chute;
         cout << "Tentativa : " <<tentativas <<endl;
         cout << "Qual o seu chute  ?" << endl;
@@ -30,9 +65,12 @@ int main (){
         bool acertou = (chute == NUM_SECRET);
         bool maior = (chute > NUM_SECRET);
 
+        /*CONDICOES*/
+
         if (acertou){
             cout << "Parabens voce acertou o numero secreto !" << endl;
             nao_acertou = false;
+            break;
         }
         else if (maior){
             cout << "O seu chute foi maior que o numero secreto !" << endl;
@@ -42,8 +80,18 @@ int main (){
         }
     }
     cout << "Fim de Jogo !"<<endl;
-    cout << "Voce acertou o numero secreto em : "<<tentativas <<" tentativas."<<endl;
+    if (nao_acertou){
+        cout << "Voce perdeu tente novamente" <<endl;
+    }
+    else{
+    cout << "Voce acertou o numero secreto em : "<<tentativas-- <<" tentativas."<<endl;
     cout.precision(2);
     cout << fixed;
     cout << "Sua pontuacao foi de : " <<pontos <<endl;
+    }
+
+    /*PARA O PROGRAMA NAO PARAR DE RODAR RETIRAR O :
+    return 0;
+    */
+    return 0;
 }
